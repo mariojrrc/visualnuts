60ac2d790653fa7ca68aeb85d97e6da5a5e11bb0
Mario Costa <mariojr.rcosta@gmail.com>
2022-06-11 00:13:29 -0300

    ci(sonarqube): add coverage report

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
     coverageProvider: 'v8',
     testEnvironment: 'node',
     testMatch: ['**/tests/**/*.test.js?(x)', '**/tests/**/**/*.test.js?(x)'],
+    testResultsProcessor: "jest-sonar-reporter"
 };
